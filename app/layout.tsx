6c4fe0fc00acea8f7116e68bbcef4d0b7d04ecd3
Aaaditya-03 <adityapaswan645@gmail.com>
2025-12-05 16:02:46 +0530

    Added Navbar & Setup Light and Dark Theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import Navbar from "@/components/navigation/navbar";
 
 const inter = Inter({
 	subsets: [
@@ -40,6 +41,7 @@ export default function RootLayout({
 					enableSystem
 					disableTransitionOnChange
 				>
+					<Navbar />
 					{children}
 				</ThemeProvider>
 			</body>
